0c9b801a918196e122dfe0d5d50a110a97f2198a
sanjit-koovers <sanjit.k@koovers.in>
2025-10-09 16:33:25 +0530

    onpage seo added

--- src/app/dalalfolios/page.tsx
+++ src/app/dalalfolios/page.tsx
@@ -3,42 +3,13 @@ import FAQ from '@/components/streetfolios/FAQ'
 import InvestmentSteps from '@/components/streetfolios/InvestmentSteps'
 import ModelPortfolios from '@/components/streetfolios/ModelPortfolios'
 import WhyChoosePortfolios from '@/components/streetfolios/WhyChoose'
-import Head from 'next/head'
 
-export default function page({
-  pageTitle,
-  pageDescription,
-  pagePath,
-  ogImage,
-}: {
-  pageTitle: string;
-  pageDescription: string;
-  pagePath: string;
-  ogImage?: string;
-}) {
+
+export default function page() {
    const canonicalUrl = `https://thedalalstreethub.com${pagePath}`;
   return (
     <>
-    <Head>
-        <title>{pageTitle} | The Dalal Street Hub</title>
-        <meta name="description" content={pageDescription} />
-        <meta name="robots" content="index, follow" />
-        <link rel="canonical" href={canonicalUrl} />
-
-        {/* Open Graph */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={canonicalUrl} />
-        <meta property="og:title" content={pageTitle} />
-        <meta property="og:description" content={pageDescription} />
-        {ogImage && <meta property="og:image" content={ogImage} />}
-
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content={canonicalUrl} />
-        <meta name="twitter:title" content={pageTitle} />
-        <meta name="twitter:description" content={pageDescription} />
-        {ogImage && <meta name="twitter:image" content={ogImage} />}
-      </Head>
+    
       <CuratedPortfolioSection/>
       <WhyChoosePortfolios/>
       <InvestmentSteps/>
