bf8b99b5f0aed987110db99777ea285e3de79c8b
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2025-10-09 23:45:36 +0530

    images added on teams

--- src/components/home/TeamSpace.tsx
+++ src/components/home/TeamSpace.tsx
@@ -6,12 +6,25 @@ export default function TeamSpace() {
 
         <div className="flex flex-col md:flex-row w-full gap-6">
         {/* Large image */}
-        <div className="w-full md:w-3/4">
+        <div className="w-full md:w-3/4 flex flex-col gap-6">
             <img
             src="/Team.jpg"
             alt="team"
-            className="rounded-xl w-full h-80 sm:h-80 md:h-[39rem] object-cover"
+            className="rounded-xl w-full h-80 md:h-[29rem] object-cover"
             />
+            <div className="flex md:flex-row flex-col gap-6">
+            <img
+            src="/Team4.jpeg"
+            alt="team"
+            className="rounded-xl w-full h-80 md:h-[16rem] object-cover"
+            />
+            <img
+            src="/Team5.jpeg"
+            alt="team"
+            className="rounded-xl w-full h-80 md:h-[16rem] object-cover"
+            />
+
+            </div>
         </div>
 
         {/* Small images */}
@@ -19,17 +32,17 @@ export default function TeamSpace() {
             <img
             src="/Team1.jpeg"
             alt="team"
-            className="rounded-xl w-full h-80 md:h-[12rem] object-cover"
+            className="rounded-xl w-full h-80 md:h-[14.5rem] object-cover"
             />
             <img
             src="/Team2.jpeg"
             alt="team"
-            className="rounded-xl w-full h-80 md:h-[12rem] object-cover"
+            className="rounded-xl w-full h-80 md:h-[14.5rem] object-cover"
             />
             <img
             src="/Team3.jpeg"
             alt="team"
-            className="rounded-xl w-full h-80 md:h-[12rem] object-cover"
+            className="rounded-xl w-full h-80 md:h-[14.5rem] object-cover"
             />
         </div>
         </div>
